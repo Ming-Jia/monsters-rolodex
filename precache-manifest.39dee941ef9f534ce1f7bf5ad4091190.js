1e822806cb97718f3157b7fd2cf973ae9f2e84a4
Yong Ming-Jia <yongmingjia0112@gmail.com>
2020-05-30 19:12:56 +0800

    Updates

---
+++ precache-manifest.39dee941ef9f534ce1f7bf5ad4091190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b13cfa64cbcc0fd562580b4db27d2b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ee2f246372b7a81daae",
+    "url": "/monsters-rolodex/static/css/main.43dd6c04.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee2f246372b7a81daae",
+    "url": "/monsters-rolodex/static/js/main.68610413.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
